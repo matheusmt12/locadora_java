ff9a0724071501d742cbb250c120c1f2657a49ad
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2024-08-11 23:13:29 -0300

    index.html teste

--- src/main/java/com/matheus/locadoraCarro/security/SecurityConfig.java
+++ src/main/java/com/matheus/locadoraCarro/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.matheus.locadoraCarro.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.prepost.PostAuthorize;
@@ -10,11 +11,29 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.matheus.locadoraCarro.security.SecurityConfig.JwtAuthenticationFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private CustomUserDetailService userDetailService;
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -23,18 +42,39 @@ public class SecurityConfig {
 	public SecurityFilterChain config (HttpSecurity http) throws Exception{
 		
 	      http
-	        .cors(cors->cors.disable())
 	        .csrf(csrf->csrf.disable())
 	        .authorizeHttpRequests(request -> request
-	                .requestMatchers("/marca").permitAll()
 	                .requestMatchers("/modelo").permitAll()
 	                .requestMatchers("/login").permitAll()
 	                .requestMatchers("/sing-up").permitAll()
 	                .anyRequest().authenticated())
 
 	        .sessionManagement((session)-> session
-	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+
 	      return http.build();
 	}
+
+        // Configure o filtro para verificar o token
+    public OncePerRequestFilter jwtFilter(){
+        return new JwtAuthFilter(jwtService, userDetailService);
+    }
+
+
+
+	   @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://127.0.0.1:5500")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
 	
 }
